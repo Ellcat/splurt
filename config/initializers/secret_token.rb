9f7921e58f8a584e64814eec79126f73523a6a6f
Christine Ellis <jadeapple@gmail.com>
2016-09-26 17:54:41 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Splurt::Application.config.secret_key_base = '5b86297268914e5b38e3b0b7f4cf6c5e0b7b53fe7775c7c6124e67b58bc1895714c0bb87e7db5672a8fff4490a7cb511205fcb571efbaafa2818fe8f0c85622d'
